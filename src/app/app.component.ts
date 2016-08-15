42fd337196d0aca09d8ab8b83ca70dbd59c16e35
Radek Benes <radekbenes@centrum.cz>
2016-08-15 14:03:05 +0200

    15-08-16

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
-import { AngularFire, FirebaseListObservable } from 'angularfire2';
-
-declare var gapi: any;
+import { AngularFire, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2';
 
 @Component({
   moduleId: module.id,
@@ -11,9 +9,12 @@ declare var gapi: any;
 })
 export class AppComponent {
   requests: FirebaseListObservable<any[]>;
+  info: any;
 
   constructor(public af: AngularFire) {
     this.requests = af.database.list('/requests');
+    this.info = af.auth.subscribe(auth => auth);
+    console.log(this.info);
   }
 
   login() {
